902435f6079a127c6f569388006f24dffce9d27e
itz-mohanmadhav-18 <mohanmadhav448@outlook.com>
2025-05-14 10:22:50 +0530

    my-first-code

---
+++ java_basics/Day1.java
@@ -0,0 +1,20 @@
+package java_basics;
+
+public class Day1 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+    }
+
+    // This is a single-line comment
+
+    /*
+     * This is a multi-line comment
+     * that spans multiple lines.
+     */
+
+    /**
+     * This is a Javadoc comment.
+     * It can be used to generate documentation.
+     */
+}
